61b5ab6c1a908cf3325d947766dadae41e09bfa5
Shan-Wei Hsu <ericshe21110@gmail.com>
2016-07-14 20:01:33 +0800

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,36 @@
+<<!DOCTYPE html>
+<html lang = en>
+<head>
+	<meta charset=utf-8>
+	<meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name=description content="">
+    <meta name=author content="LuYH">
+    <link rel=icon href=/Content/AssetsBS3/img/favicon.ico>
+    <title>妙華如發訂單系統-首頁</title>
+    <link href="bootstrap/js/bootstrap.min.js" rel=stylesheet>
+	<link href="frontpage.css" rel=stylesheet>
+</head>
+<body>
+	<div class = Masthead>
+		<div class = Head>
+			
+		</div>
+		<div class= Menu>
+			
+		</div>
+	</div>
+	<div class = Innercover>
+		<div class = Workremind>
+			
+		</div>
+		<div class = Monthlytask>
+			
+		</div>
+	</div>
+	<div class = Mastfoot>
+		
+	</div>
+
+</body>
+</html>
