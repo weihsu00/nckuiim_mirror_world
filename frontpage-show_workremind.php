01d8745b0d8ae4e25437ca64cb1a54b76c94e3ba
Shan-Wei Hsu <ericshe21110@gmail.com>
2016-07-26 13:43:06 +0800

    Add files via upload

--- frontpage-show_workremind.php
+++ frontpage-show_workremind.php
@@ -1,71 +1,60 @@
 <?php
 $error = ""; $result = ""; 
 if (isset($_POST["send"])) {  // 是否是表單送回
-    $title = $_POST["title"];
-    $reciever = (int)$_POST["reciever"];
-    $reciever_result="";
+    $title = $_POST["title"];   // 取得表單欄位值
+    $reciever = $_POST["reciever"];
+    $reciever_result = "";
     switch ($reciever) {
-            case 0:
-                $reciever_result="0";
-                break;
-            case 1:
-                $reciever_result="1";
-                break;
-            default:
-                $reciever_result="0";
-                break;
-        }
-    $type = (int)$_POST["type"];
-    $type_result="";
-        switch ($type) {
-            case 0:
-                $type_result="0";
-                break;
-            case 1:
-                $type_result="1";
-                break;
-            default:
-                $type_result="0";
-                break;
-        }
-    $delivery_date = $_POST["delivery_date"];
-    $content = $_POST["content"];
-    if (empty($title)) { // 欄位沒填
-        $error = "標題不可是空白<br/>";
+        case 0:
+            $reciever_result = "0";  
+            break;
+        case 1:
+            $reciever_result = "1";
+            break;
     }
-    else { 
-        if ($reciever_result=="0") // 欄位沒填
-        $error .= "受文者不可是空白<br/>";   
-        else { // 表單處理
-        $db = mysqli_connect("localhost", "root", "xxxg00w0");
+    $type = $_POST["type"];
+    $type_result = "";
+    switch ($type) {
+        case 0:
+            $type_result = "0";
+            break;
+        case 1:
+            $type_result = "1";
+            break;
+    }
+    $delivery_date = $_POST["delivery_date"];
+    $content = $_POST["text_content"];
+
+    if (empty($title)) {
+        $error .= "請填入標題<br/>";
+    }   else if ($reciever==0){
+        $error .= "請選擇受文者<br/>";
+    }   else if ($type==0){
+        $error .= "請選擇類型<br/>";
+    }   else if (empty($delivery_date)){
+        $error .= "請輸入指定完成日<br/>";
+    }   else if ($content==0){
+        $error .= "請輸入內文<br/>";
+    }
+    else{
+        $db = @mysqli_connect("localhost", "root", "xxxg00w0");
         mysqli_select_db($db, "mirrorworld"); // 選擇資料庫
         $sql = "INSERT INTO instruct" .
-        "(Title, Account_ID_Reciever, Type_ID, Delivery_Date, Content)" .
-        "VALUES ('$title', '$reciever_result', '$type_result', '$delivery_date', '$content')";
-        }
-        if (!mysqli_query($db, $sql)) { // 執行SQL指令
+        "(Title, Account_ID_Reciever, Type_ID, Delivery_date, Content)" .
+        "VALUES ('$title', '$reciever_result', '$type_result', '$delivery_date', '$text_content')";
+        if (!mysqli_query($db, $sql)) {
             $result = "新增記錄失敗...<br/>" . mysqli_error($db);
         }
         else $result = "新增記錄成功...<br/>";
-        mysqli_close($db); // 關閉連接         
+        mysqli_close($db);
     }
-   
 }
+
 else {  // 初始表單欄位值
-    $title = ""; $reciever = "";
-    $type = ""; $delivery_date = ""; $content = "";
+   $title = ""; $reciever_result = ""; $reciever = ""; $type_result = ""; $type = ""; 
+   $delivery_date = ""; $content = ""; 
 } 
 
-/*function show_work_remind(){
-    $db = mysqli_connect("localhost", "root", "xxxg00w0");
-if (!$db) die("錯誤: 無法連接MySQL伺服器!" . mysqli_connect_error());
-mysqli_select_db($db, "mirrorworld") or  // 選擇資料庫
-    die("錯誤: 無法選擇資料庫!" . mysqli_error($db));
-$sql = "SELECT * FROM instruct";
-$rows = mysqli_query($db, $sql); // 執行SQL查詢              
-$num = mysqli_num_rows($rows); // 取得記錄數
-mysqli_close($db); // 關閉伺服器連接
-}*/
 ?>
 
 <!DOCTYPE html>
@@ -113,7 +102,8 @@ mysqli_close($db); // 關閉伺服器連接
             <div class="Workremind">
                 <h3>交辦事項</h3>
                 <div class="content">
-                    <form id="instruct_form" class="form" name="workremind_form" action="">
+                <div style="color: red"><?php echo $error ?></div>
+                    <form id="instruct_form" class="form" name="workremind_form" action="" method="post">
                         <div class="fl-column1">
                             <div class="fl-name">
                                 <label>標題</label>
@@ -125,7 +115,7 @@ mysqli_close($db); // 關閉伺服器連接
                                 <label>受文者</label>
                             </div>
                             <div>
-                                <select name="reciever">
+                                <select name="reciever" id="reciever">
                                     <option value="0">請選擇受文者
                                     </option>
                                     <option value="1">1
@@ -136,7 +126,7 @@ mysqli_close($db); // 關閉伺服器連接
                                 <label>類型</label>
                             </div>
                             <div>
-                                <select name="type">
+                                <select name="type" id="type">
                                     <option value="0">請選擇類型
                                     </option>
                                     <option value="1">1
@@ -147,17 +137,17 @@ mysqli_close($db); // 關閉伺服器連接
                                 <label>指定完成日</label>
                             </div>
                             <div>
-                                <input type="text" class="form-control" name="delivery_date">
+                                <input type="text" class="form-control" name="delivery_date" id="delivery_date">
                             </div>
                         </div>
                         <div>
                             <p>內文</p>
-                            <textarea class="form-control" rows="3" name="content"></textarea>
+                            <textarea class="form-control" rows="3" name="text_content" id="text_content"></textarea>
                         </div>
-                        <input type="submit" name="send" value="送出">
-                        <input type="reset" name="notsend" value="重置">
-                        <?php echo $result ?>
+                        <input type="submit" name="send" id="send" value="送出"/>
+                        <input type="reset" name="send" id="send" value="重置"/>
                     </form>
+                    <?php echo $result ?>
                     
                     <table class="workremind_output">
                     <thead>
@@ -175,30 +165,28 @@ mysqli_close($db); // 關閉伺服器連接
                         <?php
                         $db = mysqli_connect("localhost", "root", "xxxg00w0");
                         if (!$db) die("錯誤: 無法連接MySQL伺服器!" . mysqli_connect_error());
-                        mysqli_select_db($db, "mirrorworld") or  // 選擇資料庫
-                        die("錯誤: 無法選擇資料庫!" . mysqli_error($db));
-                        $sql = "SELECT * FROM instruct";
-                        $rows = mysqli_query($db, $sql); // 執行SQL查詢              
-                        $num = mysqli_num_rows($rows); // 取得記錄數
-                        mysqli_close($db); // 關閉伺服器連接
-                        if ($num > 0) { // 有記錄
-                        // 顯示每一筆記錄  
+                            mysqli_select_db($db, "mirrorworld") or  // 選擇資料庫
+                                die("錯誤: 無法選擇資料庫!" . mysqli_error($db));
+                            $sql = "SELECT * FROM instruct";
+                            $rows = mysqli_query($db, $sql); // 執行SQL查詢              
+                            $num = mysqli_num_rows($rows); // 取得記錄數
+                            mysqli_close($db); // 關閉伺服器連接
+                        if ($num > 0) { // 有記錄 
                             for ($i = 0;$i < $num; $i++ ) {
-                            // 取出記錄資料
-                                $row = mysqli_fetch_row($rows);
-                                echo "<tr>";
-                                echo "<td>" . $row[0] . "</td>";
-                                echo "<td>" . $row[1] . "</td>";
-                                echo "<td>" . $row[2] . "</td>";
-                                echo "<td>" . $row[3] . "</td>";
-                                echo "<td>" . $row[4] . "</td>";
-                                echo "<td>" . "</td>";
-                                echo "<td>" . "</td>";
-                                echo "</tr>";
+                            $row = mysqli_fetch_row($rows);
+                            echo "<tr>";
+                            echo "<td>" . $row[0] . "</td>";
+                            echo "<td>" . $row[1] . "</td>";
+                            echo "<td>" . $row[2] . "</td>";
+                            echo "<td>" . $row[3] . "</td>";
+                            echo "<td></td>";
+                            echo "<td></td>";
+                            echo "<td></td>";
+                            echo "</tr>";
                             }
                         }
-                        mysqli_free_result($rows);
-                        ?>
+                    mysqli_free_result($rows);
+                    ?>
                     </tbody>
                 </table>
                 </div>
